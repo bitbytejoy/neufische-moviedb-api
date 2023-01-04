a3e7d0ada3c8dd2ee6d51363ff98d395b0b94fad
bitbytejoy <bitbytejoy@gmail.com>
2023-01-04 11:01:46 +0100

    init: BIG BANG

---
+++ src/main/java/de/neuefische/movedbapi/MovedbApiApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.movedbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovedbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovedbApiApplication.class, args);
+    }
+
+}
